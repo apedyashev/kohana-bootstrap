4735d9a21635cc4c75356589b1e2d824ffa7ccfb
Alexey Pedyashev <alexey@idealernetwork.com>
2013-11-26 19:14:09 +0400

    added exampele of logged user controller

---
+++ application/classes/Controller/Profile.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Profile managment controller
+ * 
+ * Extends My_LoggedUsersController so you can be sure that only LOGGED users  have access to actions of this controller
+ * 
+ */
+class Controller_Profile extends My_LoggedUsersController {
+
+  public function action_index(){
+    // You can be sure that $this->_loggedUser contains logged user
+  }
+} 
